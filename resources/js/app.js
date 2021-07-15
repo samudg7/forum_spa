3272b7161ad8715ac4d7fdc65423f2c63ff00a05
Samuele <61940773+samudg7@users.noreply.github.com>
2021-07-15 14:20:13 +0200

    esempio

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+import Vue from 'vue'
+import vuetify from '../plugins/vuetify' // path to vuetify export
+
+//Vue.use(Vuetify)
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
+//import router from './Router/router.js'
+
+new Vue({
+    vuetify,
+}).$mount('#app')
